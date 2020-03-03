8f845eab1c07cfa68312d8373c5c9f9b8b37a2eb
Muhammad Irwan Andriawan <irwan@andriawan.com>
2020-03-03 23:59:54 +0700

    format code

--- routes/web.php
+++ routes/web.php
@@ -17,18 +17,15 @@ Route::get('/', function () {
 
 Route::get('/pdf', function () {
     $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
-
-		$pdf->setPrintHeader(false);
-		$pdf->setPrintFooter(false);
-		$pdf->SetMargins(10, 0, 15, true);
-		$pdf->SetAuthor('TEST');
-		$pdf->SetTitle('DATA TEST');
-		$pdf->SetSubject('REPORT');
-		$pdf->SetCellPadding(0);
-
-		$pdf->AddPage();
-        $view = view('pdf')->render();
-        // $view = '<h1>test</h1>';
-		$pdf->writeHTML($view, true, 0, true, 0);
-		$pdf->Output('report.pdf', 'I');
+    $pdf->setPrintHeader(false);
+    $pdf->setPrintFooter(false);
+    $pdf->SetMargins(10, 0, 15, true);
+    $pdf->SetAuthor('TEST');
+    $pdf->SetTitle('DATA TEST');
+    $pdf->SetSubject('REPORT');
+    $pdf->SetCellPadding(0);
+    $pdf->AddPage();
+    $view = view('pdf')->render();
+    $pdf->writeHTML($view, true, 0, true, 0);
+    $pdf->Output('report.pdf', 'I');
 });
